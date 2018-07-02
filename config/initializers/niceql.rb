e22c1ef09356b9fde590e78927ef0c5fd2fb49b5
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-02 15:22:57 -0700

    test

--- config/initializers/niceql.rb
+++ config/initializers/niceql.rb
@@ -1,13 +1,37 @@
 if defined? Niceql
   Niceql.configure do |c|
-    # You can adjust pg_adapter in production at your own risk!
-    # If you need it in production use exec_niceql
-    # default: false
-    # c.pg_adapter_with_nicesql = Rails.env.development?
+    # Setting pg_adapter_with_nicesql to true will force formatting SQL queries
+      # before executing them, this will lead to better SQL-query debugging and much clearer error messages
+      # if you are using Postgresql as a data source.
+      # You can adjust pg_adapter in prooduction but do it at your own risk!
+      # If you need to debug SQL queries in production use exec_niceql
+      # default: false
+      # uncomment next string to enable in development
+      # c.pg_adapter_with_nicesql = Rails.env.development?
 
-    # this are default settings, change it to your project needs
-    c.indentation_base = 2
-    # c.open_bracket_is_newliner = false
-    c.prettify_active_record_log_output = true
+      # uncomment next string if you want to log prettified SQL inside ActiveRecord logging.
+      # default: false
+      c.prettify_active_record_log_output = true
+
+      # now error prettifying is configurable
+      # default: defined? ::ActiveRecord::Base && ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'postgresql'
+      # c.prettify_pg_errors = defined? ::ActiveRecord::Base && ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'postgresql'
+
+      # spaces count for one indentation
+      c.indentation_base = 2
+
+      # setting open_bracket_is_newliner to true will start opening brackets '(' with nested subqueries from new line
+      # i.e. SELECT * FROM ( SELECT * FROM tags ) tags; will transform to:
+      # SELECT *
+      # FROM
+      # (
+     #    SELECT * FROM tags
+     #  ) tags;
+     # when open_bracket_is_newliner is false:
+      # SELECT *
+      # FROM (
+     #   SELECT * FROM tags
+     # ) tags;
+     # default: false
   end
 end
