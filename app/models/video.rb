7147d894df947fe014ce77617edf0b2acf018258
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-03 15:09:09 -0700

    removed video category validation

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,3 @@
 class Video < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, optional: true
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,8 +1,11 @@
 describe Video do
   it 'saves itself' do
-    category = Category.create name: 'Comedy'
-    video    = Video.new title: 'Family Guy', description: 'this is Family Guy', category: category
+    # category = Category.create name: 'Comedy'
+    # video    = Video.new title: 'Family Guy', description: 'this is Family Guy', category: category
+
+    video = Video.new title: 'Family Guy', description: 'this is Family Guy'
     video.save
+
     Video.first.title.should       == 'Family Guy'
     Video.first.description.should == 'this is Family Guy'
 
