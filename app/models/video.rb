238cc82552701d970e9d3cb592d932d772e47ace
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-03 18:09:20 -0700

    added search_by_title

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,9 @@
 class Video < ApplicationRecord
-  belongs_to :category, optional: true
-
+  belongs_to            :category, optional: true
   validates_presence_of :title, :description
+
+  def self.search_by_title title
+    return [] if title.blank?
+    where('title LIKE ?', "%#{title}%").order 'created_at DESC'
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -2,4 +2,40 @@ describe Video do
   it { should belong_to :category }
   it { should validate_presence_of :title }
   it { should validate_presence_of :description }
+
+  describe 'search_by_title' do
+    it 'returns an empty array if there is no match' do
+      expect(Video.search_by_title('abc')).to eq []
+
+      video1 = Video.create title: 'Family Guy', description: 'desc'
+      expect(Video.search_by_title('abc')).to eq []
+    end
+
+    it 'returns an array of one video for an exact match' do
+      video1 = Video.create title: 'Family Guy', description: 'desc'
+      expect(Video.search_by_title('Family Guy')).to eq [video1]
+    end
+
+    it 'returns an array of one video for a partial match' do
+      video1 = Video.create title: 'Family Guy', description: 'desc'
+      video2 = Video.create title: 'Cool', description: 'desc'
+      expect(Video.search_by_title('Family')).to eq [video1]
+      expect(Video.search_by_title('family')).to eq [video1]
+      expect(Video.search_by_title('amily')).to eq [video1]
+      expect(Video.search_by_title('family guy')).to eq [video1]
+      expect(Video.search_by_title('Cool')).to eq [video2]
+    end
+
+    it 'returns an array of all matches ordered by created_at' do
+      video1 = Video.create title: 'Family Guy', description: 'desc', created_at: 1.day.ago
+      video2 = Video.create title: 'Family Man', description: 'desc'
+      expect(Video.search_by_title 'Family').to eq [video2, video1]
+    end
+
+    it 'returns an empty array for a search with an empty string' do
+      video1 = Video.create title: 'Family Guy', description: 'desc', created_at: 1.day.ago
+      video2 = Video.create title: 'Family Man', description: 'desc'
+      expect(Video.search_by_title '').to eq []
+    end
+  end
 end
