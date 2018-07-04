3dbf976af14f2af8197b29f2da20094e612dab45
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-04 09:56:47 -0700

    refactored videos controller tests

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,35 +1,32 @@
 describe VideosController do
   describe 'GET show' do
-    it 'sets the @video variable' do
-      user  = User.create email: 'a@a.com', password: 'a', full_name: 'a a'
-      video = Video.create title: 'a', description: 'aaa'
-      get :show, params: { id: video.id }, session: { user_id: user.id }
-      assigns(:video).should == video
+    it 'sets @video for authenticated users' do
+      session[:user_id] = Fabricate(:user).id
+      video = Fabricate :video
+      get :show, params: { id: video.id }
+      expect(assigns :video).to eq video
     end
 
-    it 'renders the show template' do
-      user  = User.create email: 'a@a.com', password: 'a', full_name: 'a a'
-      video = Video.create title: 'a', description: 'aaa'
-      get :show, params: { id: video.id }, session: { user_id: user.id }
-      response.should render_template :show
+    it 'redirects the user to the sign in page for unauthenticated users' do
+      video = Fabricate :video
+      get :show, params: { id: video.id }
+      expect(response).to redirect_to sign_in_path
     end
   end
 
-  describe 'GET search' do
-    it 'sets the @videos variable' do
-      user   = User.create email: 'a@a.com', password: 'a', full_name: 'a a'
-      video1 = Video.create title: 'a', description: 'desc'
-      video2 = Video.create title: 'cat', description: 'desc'
-      video3 = Video.create title: 'cob', description: 'desc'
-      get :search, params: { search_term: 'a' }, session: { user_id: user.id }
-      expect(assigns(:videos)).to include video1, video2
-      expect(assigns(:videos)).not_to include video3
+
+  describe 'Post search' do
+    it 'sets @videos for authenticated users' do
+      session[:user_id] = Fabricate(:user).id
+      futurama = Fabricate :video, title: 'Futurama'
+      get :search, params: { search_term: 'rama' }
+      expect(assigns :videos).to eq [futurama]
     end
 
-    it 'renders the search template' do
-      user = User.create email: 'a@a.com', password: 'a', full_name: 'a a'
-      get :search, params: { search_term: 'a' }, session: { user_id: user.id }
-      expect(response).to render_template :search
+    it 'redirects to sign in page for unauthenticated users' do
+      futurama = Fabricate :video, title: 'Futurama'
+      get :search, params: { search_term: 'rama' }
+      expect(response).to redirect_to sign_in_path
     end
   end
 end
