c26478e61633c833d95a0033246ade0fc656a85a
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-03 19:03:16 -0700

    added search form

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,9 @@
 class VideosController < ApplicationController
-  def index
-    # @videos = Video.all
-    # @categories = Category.all
-  end
-
   def show
     @video = Video.find params[:id]
+  end
+
+  def search
+    @videos = Video.search_by_title params[:search_term]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   get '/home',   to: 'categories#index'
   get '/videos', to: 'categories#index'
 
-  resources :videos,     only: [:show]
+  resources :videos, only: [:show] do
+    collection do
+      get 'search'
+    end
+  end
+
   resources :categories, only: [:index, :show]
 end
