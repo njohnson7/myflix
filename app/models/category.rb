970a1595db852e189121413b775b2344143d0f80
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-03 19:57:34 -0700

    added Category#recent_videos

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :videos, -> { order 'title' }
+
+  def recent_videos
+    Video.where('category_id = ?', id).order('created_at DESC').limit(6)
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -29,5 +29,35 @@ describe Category do
     category.reload
     expect(category.videos).to eq [video3, video1, video4, video2]
   end
-end
 
+  describe '#recent_videos' do
+    it 'returns [] if the category has no videos' do
+      comedy = Category.create name: 'Comedy'
+      expect(comedy.recent_videos).to eq []
+    end
+
+    it 'returns an array of all videos in reverse chronological order (most recent first) by created_at if less than six videos in category' do
+      comedy = Category.create name: 'Comedy'
+      video1 = Video.create title: 'video1', description: 'desc', category: comedy
+      video2 = Video.create title: 'video2', description: 'desc', category: comedy, created_at: 2.day.ago
+      video3 = Video.create title: 'video3', description: 'desc', category: comedy, created_at: 1.days.ago
+      video4 = Video.create title: 'video4', description: 'desc', category: comedy, created_at: 3.days.ago
+      comedy.reload
+      expect(comedy.recent_videos).to eq [video1, video3, video2, video4]
+    end
+
+    it 'returns an array of most recent six videos in category if more than six videos' do
+      comedy = Category.create name: 'Comedy'
+      video1 = Video.create title: 'video1', description: 'desc', category: comedy
+      video2 = Video.create title: 'video2', description: 'desc', category: comedy, created_at: 2.day.ago
+      video3 = Video.create title: 'video3', description: 'desc', category: comedy, created_at: 1.days.ago
+      video4 = Video.create title: 'video4', description: 'desc', category: comedy, created_at: 3.days.ago
+      video5 = Video.create title: 'video5', description: 'desc', category: comedy
+      video6 = Video.create title: 'video6', description: 'desc', category: comedy
+      video7 = Video.create title: 'video7', description: 'desc', category: comedy
+      comedy.reload
+      expect(comedy.recent_videos).to eq [video7, video6, video5, video1, video3, video2]
+      expect(comedy.recent_videos).not_to include(video4)
+    end
+  end
+end
