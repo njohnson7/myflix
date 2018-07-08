e761442cbf9e3c13f9f0ba51cdba04f94a78a0bc
Nick Johnson <njohnson7@users.noreply.github.com>
2018-07-07 18:31:21 -0700

    added feature test for follows

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -11,3 +11,7 @@ def sign_in user = Fabricate(:user)
   click_button 'Sign in'
   page.should have_content user.full_name
 end
+
+def click_on_video_on_home_page video
+  find("a[href='/videos/#{video.id}']").click
+end
